97d37a44fbc39d4beb309b79668874e0cd6b9f6e
Jacocococo <jacocoplays@gmail.com>
2024-03-14 12:58:55 +0100

    Signup error field + code cleanup

--- frontend/src/login.tsx
+++ frontend/src/login.tsx
@@ -1,11 +1,18 @@
 import { useLocation, useNavigate } from "@solidjs/router";
 import { Component } from "solid-js";
 
+interface Response {
+    success: boolean;
+    text: string;
+}
+
 export const SignUp: Component = () => {
     let username: HTMLInputElement;
     let email: HTMLInputElement;
     let password: HTMLInputElement;
     let passwordConfirm: HTMLInputElement;
+
+    let errorField: HTMLParagraphElement;
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -22,23 +29,28 @@ export const SignUp: Component = () => {
             method: "POST",
             body: data,
         })
-            .then(res => res.json())
+            .then(res => res.json() as Promise<Response>)
             .then(body => {
                 if (body.success) {
                     navigate(location.state?.prevLocation ?? "/");
                     return;
                 }
+
+                errorField.innerHTML = body.text.trimEnd(); 
             })
-            .catch(err => console.error(err));
+            .catch(() => errorField.innerHTML = "Internal server error");
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="username" ref={username} />
-            <input type="email" ref={email} />
-            <input type="password" ref={password} />
-            <input type="password" ref={passwordConfirm} />
-            <button type="submit">Sign up</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="username" ref={username} />
+                <input type="email" ref={email} />
+                <input type="password" ref={password} />
+                <input type="password" ref={passwordConfirm} />
+                <button type="submit">Sign up</button>
+            </form>
+            <p ref={errorField}></p>
+        </>
     );
 }
